acaacafbbc5cfba62738788ff665380c79103e5b
Leonardo Gomes <leogomesp10@gmail.com>
2024-03-04 23:37:35 -0300

    Simple character movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private ContactFilter2D groundFilter;
+
+    private bool _isJumping;
+
+    private Rigidbody2D _rb;
+
+    private bool IsGrounded => _rb.IsTouching(groundFilter);
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        _rb.velocity = new Vector2(moveSpeed, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (_isJumping && IsGrounded)
+        {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        _isJumping = false;
+    }
+}
